06d9a708def217d6de2d33f76c9836c19bd8b7af
Marcelo Hernandez <marcelo.hernren@hotmail.com>
2025-11-11 17:59:00 -0500

    model v0.1

--- yolo8train.py
+++ yolo8train.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
 
 def main():
-    model = YOLO("yolo8n.yaml")
+    model = YOLO("yolov8n.yaml")
 
-    data_yaml_file = r"D:\Gear-5-1.5\G5 1.5.v1i.yolov8 (1)\data.yaml"
-    project = r"D:\Gear-5-1.5\G5 1.5.v1i.yolov8 (1)"
+    data_yaml_file = r"C:\Users\marce\Gear-5-1.5\G5 1.5.v1i.yolov8 (1)\data.yaml"
+    project = r"C:\Users\marce\Gear-5-1.5\G5 1.5.v1i.yolov8 (1)"
     experiment = "My-card-model"
 
-    batch_size = 32
+    batch_size = 40
 
     results = model.train(data = data_yaml_file,
                           epochs = 100,
@@ -16,7 +16,7 @@ def main():
                           batch = batch_size,
                           device = 0,
                           patience = 5,
-                          imgsz = 640,
+                          imgsz = 720,
                           verbose = True,
                           val = True)
     
